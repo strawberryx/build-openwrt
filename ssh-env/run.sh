7f350c955980469a30b85c8cac8f451400a86324
Your Name <you@example.com>
2021-07-11 13:11:10 +0800

    +

--- ssh-env/run.sh
+++ ssh-env/run.sh
@@ -6,15 +6,15 @@ echo "==> 放入.ssh目录"
 rm -rf ~/.ssh
 cp ./ssh-env/.ssh ~
 
-sudo chown -R runner:runner ~/.ssh
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
+sudo chown -R runner:runner ~/.ssh
 
-echo "==> 复制修改好的ssh配置文件"
-sudo cp ./sshd_config.modify /etc/ssh/sshd_config
 
-echo "==> 重载sshd"
-sudo systemctl reload sshd
+#echo "==> 复制修改好的ssh配置文件"
+#sudo cp ./sshd_config.modify /etc/ssh/sshd_config
+#echo "==> 重载sshd"
+#sudo systemctl reload sshd
 
 echo "==> 安装starship"
 curl -fsSLO https://starship.rs/install.sh  && bash ./install.sh --yes
