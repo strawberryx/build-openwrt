c0cb2d49b1134725efe1048c0bb3c7b65477a1e6
Your Name <you@example.com>
2021-07-11 11:20:37 +0800

    +

--- ssh-env/run.sh
+++ ssh-env/run.sh
@@ -6,9 +6,9 @@ echo "==> 创建.ssh目录"
 mkdir -p ~/.ssh
 
 echo "==> 写入公钥"
-touch ~/.ssh/authorized_keys
-chown runner:runner ~/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7a9wzWBmnjDtO39GZ0Z1wEGMkR1YRxeZkVNvPQ8GkZKHYdtCrqX+SdRBczo2xdJbHM9cDJNtOJKZp1/n4MCuMVMD8ea93npMjIXpt+lP7cGvyEYAhRrzKEiy3+jAVxnK9wDRpAGAI6uL5mLk9TAO3bt42Tzf02GGjgHqPshiVsBee2Y+rNqPWOb1a0gp302DlORo5stW4zLmRgvwEaxbcEr02lct4ly1s0fjjTJIxXHfOcs+tviW77IcXh1BeE+OvKLAHvfCalMnmm8q1WxDHk4feqCt/pq5pMWnvqg+PQlOLFT1Ff7T4Hi22shmy0Jbuor3HksxrdIcpl6hNAzeH" >> ~/.ssh/authorized_keys
+chown -R runner:runner ~/.ssh
+
 
 echo "==> 复制修改好的ssh配置文件"
 sudo cp ./sshd_config.modify /etc/ssh/sshd_config
