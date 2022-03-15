cc9eeab5ef37d587d2fa6f738c691f53655f60ff
c3p7f2 <9e16c786@gmail.com>
2022-03-15 12:07:36 +0800

    +

--- add-package.sh
+++ add-package.sh
@@ -5,16 +5,10 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo "src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git" >> "feeds.conf.default"
 
-git pull
 
-#
+# 零散小包
 cd package
 
+git clone https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git
 # N1
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic luci-app-amlogic
-
-# 另外增加
-git clone https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git
-
-# rm -rf luci-app-openclash
-# git clone https://github.com/vernesong/OpenClash.git
