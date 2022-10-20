7e5d34a3b4375d674857159eea4fa054ca01fd68
c3p7f2 <shenlebanshashi@gmail.com>
2022-10-20 21:59:02 +0800

    +

--- diy-2.sh
+++ diy-2.sh
@@ -9,7 +9,7 @@
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='HarmonyOS'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/HarmonyOS /Built on $(TZ=UTC-8 date "+%Y.%m.%d") @ HarmonyOS /g" package/lean/default-settings/files/zzz-default-settings
+# sed -i "s/HarmonyOS /Built on $(TZ=UTC-8 date "+%Y.%m.%d") @ HarmonyOS /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
