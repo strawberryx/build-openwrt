6949e364649050d63a495129d48a8e92d8446370
c3p7f2 <78227341+c3p7f2@users.noreply.github.com>
2023-06-08 13:10:48 +0800

    editorTextarea

--- tz.js
+++ tz.js
@@ -264,10 +264,10 @@ var buttonRecom = createButton("填推荐", function () {
     let output = input.replace(regex, replacement);
     // 获取textarea和body元素
     let textarea = document.getElementById("description");
-    let body = document.getElementsByClassName("ke-edit-textarea")[0];
+    let editorTextarea = document.getElementsByClassName("ke-edit-textarea")[0];
     // 将替换后的字符串填入元素中
     textarea.value = output;
-    body.innerHTML = output;
+    editorTextarea.value = output;
   } catch (e) {
     alert(e);
   }
