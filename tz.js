5a8cd96965a04b07406863ec39c3b169f27311ec
c3p7f2 <78227341+c3p7f2@users.noreply.github.com>
2023-06-08 10:22:52 +0800

    详细标注功能

--- tz.js
+++ tz.js
@@ -268,7 +268,7 @@ div.appendChild(buttonSoft);
 
 
 // 使用函数创建发布按钮
-var buttonSoft = createButton("发布", function () {
+var buttonSoft = createButton("发布&日期更新", function () {
   document.getElementsByName("isUpdatetime")[0].checked = true;
   
   document.getElementsByName("isPublish")[0].checked = true;
