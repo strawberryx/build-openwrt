fbdd0cfa544890d80c8aef713c82581ed06578e3
c3p7f2 <shenlebanshashi@gmail.com>
2023-08-29 00:21:17 +0800

    sys: Add istoreos PKG

--- scripts/openwrt/add-package.sh
+++ scripts/openwrt/add-package.sh
@@ -11,3 +11,8 @@ cd package
 # git clone https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git
 # N1
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic luci-app-amlogic
+
+# istoreos pkg
+sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
+sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
