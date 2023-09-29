1ca1ed2ce5253f517272c0b92621d7a9ca6dd4cb
c3p7f2 <shenlebanshashi@gmail.com>
2023-09-29 12:54:15 +0800

    更新 rustup & 调整默认语言

--- scripts/openwrt/vektor-exec.sh
+++ scripts/openwrt/vektor-exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -uo pipefail
 
-# 返回到能编译阿里云webdav的makefile
-curl -o ./feeds/packages/multimedia/aliyundrive-webdav/Makefile https://raw.githubusercontent.com/coolsnowwolf/packages/aea60b5432fad984c0a4013bad0f0c5e00dcd115/multimedia/aliyundrive-webdav/Makefile
+rustup update
+sed -i 's/option lang auto/option lang zh_cn/' feeds/luci/modules/luci-base/root/etc/config/luci
